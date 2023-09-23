812d9cfe47621ff545398f6f6c562ddc5023338b
Akanimo Michael Asuquo <101297309+Akanimomichael@users.noreply.github.com>
2023-09-24 00:14:46 +0100

    cool

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -62,6 +62,7 @@ const App = () => {
             </div>
 
             <div>
+              <ThemeSettings />
               <Routes>
                 {/* DASHBOARD */}
                 <Route path="/" element={<Ecommerce />} />
